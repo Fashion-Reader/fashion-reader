071ec22ef8fecdced47e05795912711c89b1f209
EC2 Default User <ec2-user@ip-172-31-7-6.ap-northeast-2.compute.internal>
2021-10-27 15:04:11 +0000

    :sparkles: Add answer to user query

--- 02_RestAPI_server/fashion-reader/api_prod/views.py
+++ 02_RestAPI_server/fashion-reader/api_prod/views.py
@@ -6,36 +6,138 @@ from rest_framework import status
 from .models import Products
 import urllib.parse
 from .classifier import QueryModule
-
-query_module = QueryModule()
-
-
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import pandas as pd
+from copy import deepcopy
+from sklearn.metrics import f1_score
+from torch import nn, optim
+from torch.cuda.amp import autocast, GradScaler
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from tqdm import tqdm
+from sklearn.metrics import classification_report
 from sentence_transformers import SentenceTransformer
 import numpy as np
 from numpy import dot
 from numpy.linalg import norm
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments, ElectraForSequenceClassification, AdamW, get_cosine_with_hard_restarts_schedule_with_warmup
+import pymysql
+from pymongo import MongoClient
 
-def cos_sim(A, B):
-    return dot(A, B)/(norm(A)*norm(B))
+names = {0: '이름',
+         1: '가격',
+         2: '사이즈 옵션',
+         3: '소재',
+         4: '비침',
+         5: '카테고리',
+         6: '색 옵션',
+         7: '두께감',
+         8: '신축성',
+         9: '촉감',
+         10: '핏',
+         11: '안감',
+         12: '스타일',
+         13: '프린팅',
+         14: '상의 색',
+         15: '하의 색',
+         16: '상의 카테고리',
+         17: '하의 카테고리',
+         18: '넥 라인'}
 
-def similarity_comparison(query):
-    sentences = ["소매기장", "소재", "색깔", "프린팅", "핏", "카테고리", "기장", "넥라인", "스타일"]
-    # model = SentenceTransformer("sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens")
-    model = SentenceTransformer("sentence-transformers/paraphrase-xlm-r-multilingual-v1")
+name2column = {
+    "이름": ("products", "name"),
+    "가격": ("products", "price"),
+    "사이즈 옵션": ("products", "size_options"),
+    # "소재": (""),
+    "비침": ("products", "see_through"),
+    "카테고리": ("products", "item_type"),
+    "색 옵션": ("products", "color_options"),
+    "두께감": ("products", "thickness"),
+    "신축성": ("products", "flexibility"),
+    "촉감": ("products", "touch"),
+    "핏": ("products", "fit"),
+    "안감": ("products", "lining"),
+    "스타일": ("vqa_table", "style"),
+    "프린팅": ("vqa_table", "printing"),
+    "상의 색": ("vqa_table", "top_color"),
+    "하의 색": ("vqa_table", "bottom_color"),
+    "상의 카테고리": ("vqa_table", "top_category"),
+    "하의 카테고리": ("vqa_table", "bottom_category"),
+    "넥 라인": ("vqa_table", "neckline"),
+}
 
+# Load model
+network = torch.load("/home/ec2-user/fashion_reader/03_Model/QA_model.pt", map_location=torch.device('cpu'))
+pretrained_model_state = deepcopy(network.state_dict())
+network.load_state_dict(pretrained_model_state)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+network.to(device)
+network.eval()
 
-    query_embedding = model.encode(query)
-    embeddings = model.encode(sentences)
+# CONNECT DB
+db = pymysql.connect(host='database.cawpd3yaf0pl.us-east-2.rds.amazonaws.com',
+                     user='admin',
+                     passwd='00000000')
+cursor = db.cursor()
 
-    result = {}
+# USE DATABASE
+SQL = "use products_table"
+cursor.execute(SQL)
 
-    for idx, embedding in enumerate(embeddings):
-        print(query, sentences[idx])
-        print(cos_sim(query_embedding, embedding))
-        print()
-        result[sentences[idx]] = cos_sim(query_embedding, embedding)
+class CFG:
+    tokenizer_max_length = 35
+    batch_size = 1
+    model_name = "xlm-roberta-base"
 
-    return sorted(result.items(), key=lambda x: x[1], reverse=True)[0][0]
+class NLP_Dataset_test(Dataset):
+    def __init__(self, dataframe, tokenizer):
+        self.dataset = dataframe
+        self.question = dataframe['question']
+        self.labels = dataframe['label']
+        self.tokenizer = tokenizer
+
+    def __getitem__(self, idx):
+        tokenized_text = self.tokenizer(self.question[idx],
+                                        max_length=CFG.tokenizer_max_length,
+                                        padding='max_length',
+                                        truncation=True,
+                                        return_tensors='pt',
+                                        add_special_tokens=True)
+        
+        tokenized_text['label'] = self.labels[idx]
+        return tokenized_text
+
+    def __len__(self):
+        return len(self.labels)
+
+def question_model(query):
+    input_str = query
+    test_df = pd.DataFrame(zip([input_str],[-1]), columns=['question', 'label'])
+    tokenizer = AutoTokenizer.from_pretrained(CFG.model_name)
+    test_set = NLP_Dataset_test(test_df, tokenizer)
+    test_loader = DataLoader(dataset=test_set, batch_size=CFG.batch_size, shuffle=False)
+
+    preds_all = []
+    prediction_array=[]
+    with tqdm(test_loader, total=test_loader.__len__(), unit='batch') as test_bar:
+        for batch in test_bar:
+            input_ids = batch['input_ids'].squeeze(1).to(device)
+            attention_mask = batch['attention_mask'].squeeze(1).to(device)
+
+            preds = network(input_ids, attention_mask = attention_mask)[0]
+            preds_all += [torch.argmax(preds, 1).detach().cpu().numpy().item()]
+
+    return names[preds_all[0]]
+
+def load_vqa_answer(item_id, columns):
+    # CHECK TABLE
+    db, column = name2column[columns]
+    SQL = f"SELECT {column} from {db} where item_id = \"{item_id}\""
+    cursor.execute(SQL)
+    return cursor.fetchall()[0][0]
 
 
 class ProdView(APIView):
@@ -53,10 +155,11 @@ class ProdView(APIView):
             user_queryset_serializer = ProductsSerializer(user_queryset, many=True)
             return Response(user_queryset_serializer.data, status=status.HTTP_200_OK)
 
+
 def query_to_response(request, **kwargs):
-    query = kwargs.get('query')
-    item_id = kwargs.get('item_id')
-    # res = query_module.estimation(query, item_id)
-    res = similarity_comparison(query)
+    query = kwargs.get("query")
+    item_id = kwargs.get("item_id")
+    column = question_model(query)
+    res = load_vqa_answer(item_id, column)
     print(res)
-    return JsonResponse({'query':query, 'response':res}, json_dumps_params = {'ensure_ascii': True})
+    return JsonResponse({'query':query, 'response':column + " : " + res}, json_dumps_params = {'ensure_ascii': True})
